92d6135650f55351a7536f30a31e16c598cf142f
James Jeffers <jdjeffers@gmail.com>
2012-07-04 21:50:24 -0400

    A little test refactor, using objects and not fields of those objects.

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -7,9 +7,9 @@ public class MoneyTest extends TestCase {
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
 		Dollar product = five.times(2);
-		assertEquals(10, product.amount);
+		assertEquals(new Dollar(10), product);
 		product = five.times(3);
-		assertEquals(15, product.amount);
+		assertEquals(new Dollar(15), product);
 	}
 
 	public void testEquality() {

655f59b181c0cf3f5ab78a7589cc61b1c44d1c03
James Jeffers <jdjeffers@gmail.com>
2012-07-04 21:55:45 -0400

    Franc? What's a Franc?

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -14,4 +14,10 @@ public class MoneyTest extends TestCase {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
+
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
 }

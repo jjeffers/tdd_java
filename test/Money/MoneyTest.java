afa8b65d0010287f77fce4c057a9697251510ce8
James Jeffers <jdjeffers@gmail.com>
2012-07-05 13:03:16 -0400

    Removing extraneous references to Franc.

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -14,15 +14,7 @@ public class MoneyTest extends TestCase {
 
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
-	}
-
-	public void testFrancMultiplication() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
 	}
 
 	public void testCurrency() {
@@ -31,7 +23,7 @@ public class MoneyTest extends TestCase {
 	}
 
 	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+		assertTrue(new Money(10, "CHF").equals(new Money(10, "CHF")));
 	}
 
 }

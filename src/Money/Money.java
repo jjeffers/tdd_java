56e68f5dbd964c4b7ba0eb1707c06d0a8dadf6c6
James Jeffers <jdjeffers@gmail.com>
2012-07-05 12:50:47 -0400

    Now equality test works as expected. We are returning the right types.

--- src/Money/Money.java
+++ src/Money/Money.java
@@ -3,33 +3,30 @@ package Money;
 public class Money {
 	protected int amount;
 	protected String currency;
-	
-	Money(int amount, String currency)
-	{
+
+	Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
-	
+
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency().equals(money.currency());
 	}
 
 	static Money dollar(int amount) {
 		return new Dollar(amount, "USD");
 	}
-	
+
 	static Money franc(int amount) {
 		return new Franc(amount, "CHF");
 	}
-	
-	Money times(int multiplier)
-	{
+
+	Money times(int multiplier) {
 		return null;
 	}
-	
-	String currency()
-	{
+
+	String currency() {
 		return currency;
 	}
 }

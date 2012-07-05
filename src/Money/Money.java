eb99bc51dbaf89a184b20481a2d7507bbf63068b
James Jeffers <jdjeffers@gmail.com>
2012-07-05 10:01:51 -0400

    And now we fix the factory method itself...

--- src/Money/Money.java
+++ src/Money/Money.java
@@ -8,7 +8,7 @@ abstract class Money {
 		return amount == money.amount && getClass().equals(money.getClass());
 	}
 
-	static Dollar dollar(int amount) {
+	static Money dollar(int amount) {
 		return new Dollar(amount);
 	}
 	

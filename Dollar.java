e504b77e214ccffa3595c76dfaf36579a5a6e996
James Jeffers <jdjeffers@gmail.com>
2012-07-04 21:09:12 -0400

    Added a constructor.

--- Dollar.java
+++ Dollar.java
@@ -1,4 +1,7 @@
 public class Dollar
 {
-    
+    public Dollar(int amount)
+    {
+        
+    }
 }
--- Money.java
+++ Money.java
@@ -1,5 +1,12 @@
-public void testMultiplication() {
-   Dollar five= new Dollar(5);
-   five.times(2);
-   assertEquals(10, five.amount);
+public class Money
+{
+
+
+    public void testMultiplication() 
+    {
+        Dollar five= new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
 }

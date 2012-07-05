18428b480a0dcd1a11ce355d83008e5323c562f2
James Jeffers <jdjeffers@gmail.com>
2012-07-04 21:52:31 -0400

    Get rid of the temporary variables in the test.

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -6,10 +6,8 @@ public class MoneyTest extends TestCase {
 
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		Dollar product = five.times(2);
-		assertEquals(new Dollar(10), product);
-		product = five.times(3);
-		assertEquals(new Dollar(15), product);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 
 	public void testEquality() {

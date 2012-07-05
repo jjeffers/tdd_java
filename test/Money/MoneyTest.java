6383b3a1b6a7e6938702d5d404722c3f8a582d76
James Jeffers <jdjeffers@gmail.com>
2012-07-05 10:17:09 -0400

    And these too.

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -12,8 +12,8 @@ public class MoneyTest extends TestCase {
 
 	public void testEquality() {
 
-		assertTrue(Money.dollar(5).equals(new Dollar(5)));
-		assertFalse(Money.dollar(5).equals(new Dollar(6)));
+		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(new Franc(5).equals(new Franc(5)));
 		assertFalse(new Franc(5).equals(new Franc(6)));
 		assertFalse(new Franc(5).equals(Money.dollar(5)));

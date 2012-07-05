0b95ff3ba3ef6e9801bc8c88362452813402b57a
James Jeffers <jdjeffers@gmail.com>
2012-07-05 09:58:42 -0400

    Implment the factory in Money itself. Kinda smelly. A base class references a derived class? Yuck.

--- src/Money/Money.java
+++ src/Money/Money.java
@@ -7,4 +7,8 @@ public class Money {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
 	}
+
+	static Dollar dollar(int amount) {
+		return new Dollar(amount);
+	}
 }

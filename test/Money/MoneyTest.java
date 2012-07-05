c0b09892ba7b136d4c033ba7a9de0f89696a04ab
James Jeffers <jdjeffers@gmail.com>
2012-07-05 12:21:12 -0400

    Add failing test for currency comparisons.

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -20,8 +20,14 @@ public class MoneyTest extends TestCase {
 	}
 
 	public void testFrancMultiplication() {
-		Money five =  Money.franc(5);
+		Money five = Money.franc(5);
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
 	}
+
+	public void testCurrency() {
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+
 }

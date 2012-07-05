f05907d3ffc58fd78effe663d634bf2a223e0e25
James Jeffers <jdjeffers@gmail.com>
2012-07-04 21:38:41 -0400

    Refactor to elimnate duplication in amount.

--- src/Money/Dollar.java
+++ src/Money/Dollar.java
@@ -6,11 +6,11 @@ public class Dollar
     
     public Dollar(int amount)
     {
-        
+    	this.amount = amount;
     }
     
     public void times(int multiplier)
     {
-        
+        amount = amount*multiplier;
     }
 }

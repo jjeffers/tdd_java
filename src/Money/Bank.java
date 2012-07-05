3b8be61e4b9b1efb1ff13d3b9308d928ba797d21
James Jeffers <jdjeffers@gmail.com>
2012-07-05 13:37:51 -0400

    Make it pass.

--- src/Money/Bank.java
+++ src/Money/Bank.java
@@ -1,7 +1,10 @@
 package Money;
 
 public class Bank {
+
 	Money reduce(Expression source, String to) {
+		if (source instanceof Money)
+			return (Money) source;
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}

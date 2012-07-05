fd15eecac5356d4bc3f6a554e8064c1b0a7f3eb4
James Jeffers <jdjeffers@gmail.com>
2012-07-05 12:42:28 -0400

    Pushing ctor implementions to base class. Much nicer.

--- src/Money/Money.java
+++ src/Money/Money.java
@@ -4,6 +4,12 @@ abstract class Money {
 	protected int amount;
 	protected String currency;
 	
+	Money(int amount, String currency)
+	{
+		this.amount = amount;
+		this.currency = currency;
+	}
+	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());

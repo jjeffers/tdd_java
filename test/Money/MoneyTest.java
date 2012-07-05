557059f76e7cb5511c79e1ceced60f352f3f2f3d
James Jeffers <jdjeffers@gmail.com>
2012-07-05 13:10:17 -0400

    This is how I'd like to write the addition test...

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -27,7 +27,10 @@ public class MoneyTest extends TestCase {
 	}
 
 	public void testSimpleAddition() {
-		Money sum = Money.dollar(5).plus(Money.dollar(5));
-		assertEquals(Money.dollar(10), sum);
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(10), reduced);
 	}
 }

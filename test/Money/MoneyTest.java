a6eea845d2c7c74f82626df34e50722de3aa5544
James Jeffers <jdjeffers@gmail.com>
2012-07-08 21:05:42 -0400

    A new test, that fails. Success.

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -74,4 +74,14 @@ public class MoneyTest extends TestCase {
 		Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
 		assertEquals(Money.dollar(10), result);
 	}
+
+	public void testSumPlusMoney() {
+		Expression fiveBucks = Money.dollar(5);
+		Expression tenFrancs = Money.franc(10);
+		Bank bank = new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(15), result);
+	}
 }

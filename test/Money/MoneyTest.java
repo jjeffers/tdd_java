d2296b64919ba219910635b3a3cccc979959dae0
James Jeffers <jdjeffers@gmail.com>
2012-07-08 21:02:28 -0400

    Oh, so FiveBucks is not really ready yet...

--- test/Money/MoneyTest.java
+++ test/Money/MoneyTest.java
@@ -67,7 +67,7 @@ public class MoneyTest extends TestCase {
 	}
 
 	public void testMixedAddition() {
-		Money fiveBucks = Money.dollar(5);
+		Expression fiveBucks = Money.dollar(5);
 		Expression tenFrancs = Money.franc(10);
 		Bank bank = new Bank();
 		bank.addRate("CHF", "USD", 2);
